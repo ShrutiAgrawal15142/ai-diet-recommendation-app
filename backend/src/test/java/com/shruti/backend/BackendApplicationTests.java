86c71abd9df4be14926ee72349551e79fd85c948
shruti <shrutiagrawal15142@gmail.com>
2025-08-03 17:56:38 +0530

    Initial commit: added backend and frontend folders

---
+++ backend/src/test/java/com/shruti/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruti.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
